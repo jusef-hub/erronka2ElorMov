f60a07c40d6de2d2d79c3c6c9f31f8b086204a67
Joseba <joseba.muruabi@elorrieta-errekamari.com>
2026-01-20 10:04:41 +0100

    Cambiar el idioma

--- app/src/main/java/com/example/elormov/ui/perfila/ProfileFragment.kt
+++ app/src/main/java/com/example/elormov/ui/perfila/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.example.elormov.ui.perfila
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,25 +13,48 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.lifecycleScope
+import com.example.elormov.R
 import com.example.elormov.databinding.FragmentProfileBinding
 import com.example.elormov.ui.login.MainActivity
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settingsDB")
 private val DARK_MODE_KEY = booleanPreferencesKey("DarkMode")
+private val LANGUAGE_KEY = stringPreferencesKey("SelectedLanguage")
 class ProfileFragment : Fragment() {
 
 	private var _binding: FragmentProfileBinding? = null
 	private val binding get() = _binding!!
+	private var selectedLanguage = "es"
 
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		initListeners()
 		observeDarkMode()
+		observeLanguage()
+	}
+
+	private fun updateUI() {
+		if (selectedLanguage == "es") {
+			binding.tvSpanish.setTextColor(resources.getColor(R.color.textPrimary, null))
+			binding.tvEnglish.setTextColor(resources.getColor(R.color.textSecondary, null))
+			binding.tvBasque.setTextColor(resources.getColor(R.color.textSecondary, null))
+			Log.i("LANGUAGE", selectedLanguage)
+		} else if (selectedLanguage == "en") {
+			binding.tvSpanish.setTextColor(resources.getColor(R.color.textSecondary, null))
+			binding.tvEnglish.setTextColor(resources.getColor(R.color.textPrimary, null))
+			binding.tvBasque.setTextColor(resources.getColor(R.color.textSecondary, null))
+		} else {
+			binding.tvSpanish.setTextColor(resources.getColor(R.color.textSecondary, null))
+			binding.tvEnglish.setTextColor(resources.getColor(R.color.textSecondary, null))
+			binding.tvBasque.setTextColor(resources.getColor(R.color.textPrimary, null))
+		}
 	}
 
 	private fun observeDarkMode() {
@@ -57,6 +81,17 @@ class ProfileFragment : Fragment() {
 		}
 	}
 
+	private fun observeLanguage() {
+		viewLifecycleOwner.lifecycleScope.launch {
+			requireContext().dataStore.data
+				.map { it[LANGUAGE_KEY] ?: "es" } // "es" por defecto
+				.collect { lang ->
+					selectedLanguage = lang
+					updateUI() // Aquí es donde se aplica el estilo visual (negritas/colores)
+				}
+		}
+	}
+
 	private fun initListeners() {
 		//Activa desactiva el darkmode y lo guarda
 		binding.swDarkMode.setOnCheckedChangeListener { buttonView, isChecked ->
@@ -64,6 +99,30 @@ class ProfileFragment : Fragment() {
 				saveDarkMode(isChecked)
 			}
 		}
+
+
+		binding.llSpanish.setOnClickListener {
+			viewLifecycleOwner.lifecycleScope.launch {
+				saveLanguage("es")
+				updateLanguage(requireContext(), "es")
+			}
+		}
+
+		binding.llEnglish.setOnClickListener {
+			viewLifecycleOwner.lifecycleScope.launch {
+				saveLanguage("en")
+				updateLanguage(requireContext(), "en")
+			}
+		}
+
+		binding.llBasque.setOnClickListener {
+			viewLifecycleOwner.lifecycleScope.launch {
+				saveLanguage("eu")
+				updateLanguage(requireContext(), "eu")
+			}
+		}
+
+
 		//Vuelve al login
 		binding.btnLogout.setOnClickListener {
 			val intent = Intent(requireContext(), MainActivity::class.java)
@@ -76,6 +135,23 @@ class ProfileFragment : Fragment() {
 		requireContext().dataStore.edit {
 			it[DARK_MODE_KEY] = value
 		}
+	}
+	private suspend fun saveLanguage(value: String) {
+		requireContext().dataStore.edit {
+			it[LANGUAGE_KEY] = value
+		}
+	}
+
+	fun updateLanguage(context: Context, languageCode: String) {
+		val locale = Locale(languageCode)
+		Locale.setDefault(locale)
+
+		val config = context.resources.configuration
+		config.setLocale(locale)
+		context.resources.updateConfiguration(config, context.resources.displayMetrics)
+
+		activity?.recreate()
+		updateUI()
 	}
 
 	override fun onCreateView(
